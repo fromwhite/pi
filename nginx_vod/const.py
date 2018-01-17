8b3c81df7265e63c73ada9e51118cc91392c9b41
fromwhite <from1white@gmail.com>
2018-01-17 17:57:33 +0800

    Update const.py

--- nginx_vod/const.py
+++ nginx_vod/const.py
@@ -1,1 +1,17 @@
+#coding:utf-8
 
+class _const:
+  class ConstError(TypeError): pass
+  class ConstCaseError(ConstError): pass
+
+  def __setattr__(self, name, value):
+      if name in self.__dict__:
+          raise self.ConstError("can't change const %s" % name)
+      if not name.isupper():
+          raise self.ConstCaseError('const name "%s" is not all uppercase' % name)
+      self.__dict__[name] = value
+
+const = _const()
+const.PI = 3.14
+const.C = 2
+const.TEMPLATE = ''
