6af550d08f93b7c2573845f183ba5045390133d8
fromwhite <from1white@gmail.com>
2018-05-18 15:20:28 +0800

    remove helper

--- ayaya/main.js
+++ ayaya/main.js
@@ -2,10 +2,42 @@ const Koa = require('koa');
 const views = require('koa-views');
 const bodyParser = require('koa-bodyparser');
 
-const router = require('./helper/router');
+const Router = require('koa-router');
+const router = Router();
+router
+    .get('/', async (ctx, next) => {
+        const title = '首页'
+        const id = 1;
+        const result = 1;
+        await ctx.render('index', {
+            title,
+            id,
+            result
+        })
+
+    })
+    .get('/lab', async (ctx, next) => {
+
+        // 测试页面 lab路由重定向sprite
+        const title = 'h5test'
+        const id = 1;
+        const papa = 1;
+        await ctx.render('sprite', {
+            title,
+            id,
+            papa
+        })
+
+    });
+
+async function logger(ctx, next) {
+
+}
 
 const app = new Koa();
 app.keys = ['Ayaya'];
+
+
 
 app.use(bodyParser());
 app.use(require('koa-static')(__dirname + '/assets/'));
