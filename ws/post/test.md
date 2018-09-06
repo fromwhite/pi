fd54f458d13b9c4b314cdf80ec0b133cf90a9f4b
fromwhite <from1white@gmail.com>
2018-09-06 15:43:26 +0800

    删除源文件

--- ws/post/test.md
+++
@@ -1,7 +0,0 @@
----
-title: 2018 test
-date: 2012-1-1
-tag: photography,live
----
-
-路过深圳 test2
--- ws/ws.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -
-export NODE_PATH=/home/pi/ws/node_modules
-cd /home/pi/ws
-/usr/bin/nodejs main.js
