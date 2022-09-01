ac3ace8bb159527ae9771a7ba4ff12af8808f8b7
fromwhite <from1white@gmail.com>
2022-09-01 16:15:49 +0800

    + formatting bak shell

--- bak.sh
+++ bak.sh
@@ -1,44 +1,34 @@
-#!/bin/sh 
+#!/bin/sh
 
-echo "* Sure you want to backup?(file)"
-while :
-do
-read input
-case $input in
-# 匹配模式
-*)
-echo "get file \033[31m $input \033[0m"
+echo "* 确认备份文档? "
+while :; do
+    read input
+    case $input in
+    *)
+        echo "get file \033[31m $input \033[0m"
 
-# 判断文件夹是否存在
-if [ ! -d "$input" ]
-then
-echo "\033[31m 检查文档,找不到 $input  \033[0m"
-exit 1
-else
-# echo "已存在"
-# 打包
-# cd $pwd/$input
-# tar -cvf backup/$input@`date +%Y-%m+%d`.tar.gz * .[!.]* 
-tar -cvpzf backup/${input}@`date +%Y-%m+%d`.tar.gz --exclude=  $input
-    if [ $? -eq 0 ]    
-    then    
-        echo "\033[32m 备份成功! \033[0m"
-        # 移除归档源文件
-        # rm -Rv $input
-        exit 0    
-    else    
-        echo "\033[31m 备份失败! \033[0m"      
-        exit 1    
-    fi
-fi
-# command...
-#tar
-exit
-# 退出本次case
-;;
-#case语句的结束
-esac
-# while
+        if [ ! -d "$input" ]; then
+            echo "\033[31m 检查文档,找不到 $input  \033[0m"
+            exit 1
+        else
+            # cd $pwd/$input
+            # tar -cvf backup/$input@`date +%Y-%m+%d`.tar.gz * .[!.]*
+            tar -cvpzf backup/${input}@$(date +%Y-%m+%d).tar.gz --exclude= $input
+            if [ $? -eq 0 ]; then
+                echo "\033[32m 备份成功! \033[0m"
+                # rm -Rv $input
+                exit 0
+            else
+                echo "\033[31m 备份失败! \033[0m"
+                exit 1
+            fi
+        fi
+        # command...
+        #tar
+        exit
+        ;;
+    esac
+    # while
 done
-# 退出shell
-exit 0 
+
+exit 0
